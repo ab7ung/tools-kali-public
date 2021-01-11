515e45e22a99b446542f68147589c7e45caec1e4
ab7ung <me@rohitjoshi.net>
2021-01-11 23:39:22 +0400

    Update abzfscan.sh

--- abzfscan.sh
+++ abzfscan.sh
@@ -3,6 +3,8 @@
 ip=$1
 echo $ip
 
+echo "sh abzfscan.sh <ip>"
+
 fullname=full-$ip.txt
 udpname=udp-top20-$ip.txt
 vulnname=vuln-$ip.txt
@@ -13,7 +15,8 @@ echo "Full tcp scan"
 nmap -Pn -p- --max-retries 1 --max-rate 500 --max-scan-delay 20 -T4 -oN $fullname $ip
 
 echo "UDP scan"
-sudo nmap -Pn -sU --top-ports 20 --max-retries 1 --open -oN $udpname $ip
+#sudo nmap -Pn -sU --top-ports 20 --max-retries 1 --open -oN $udpname $ip
+sudo nmap -sU -p- --min-rate 10000 --open -oN $udpname $ip
 
 echo "Vulnscan"
 nmap -Pn --script vuln -oN $vulnname $ip
