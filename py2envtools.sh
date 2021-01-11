d85e2d1d56461630438ea234a19632686f1d0962
ab7ung <me@rohitjoshi.net>
2021-01-11 22:40:59 +0400

    Add files via upload

---
+++ py2envtools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#enter the py2 venv before executing
+python -m pip install --upgrade pip
+python2 -m pip install --upgrade pip
+python2 -m pip install --upgrade pwntools
+
+pip2 install requests impacket progress paramiko pycrypto 
+pip2 install crackmapexec
+pip2 install aclpwn
---
+++ py3envtools.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#enter the py3 venv before executing
+python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
+
+python3 -m pip install requests progress
+pip3 install requests impacket progress paramiko pycrypto 
+pip3 install requests
+
+python3 -m pip install --upgrade pwntools
+pip3 install aclpwn
+pip3 install raccoon-scanner
+
+# incase request give error, use following commands
+#python3 -m pip install requests
+#pip install requests 
+#pip3 install requests
+#easy_install -U requests
