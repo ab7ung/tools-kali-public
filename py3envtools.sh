32a0deb2d94785d453eeba283817a9b74e714117
ab7ung <me@rohitjoshi.net>
2021-01-11 22:28:45 +0400

    Add files via upload

---
+++ py3envtools.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "py3 env"
+#py3 env setup
+
+python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
+
+
+python3 -m pip install requests progress
+pip3 install requests impacket progress paramiko pycrypto 
+
+pip3 install requests
+
+python3 -m pip install --upgrade pwntools
+
+pip3 install aclpwn
+
+pip3 install raccoon-scanner
+
+echo "done"
+#python3 -m pip install requests
+#pip install requests 
+#pip3 install requests
+#easy_install -U requests
+
