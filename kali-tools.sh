b770b0d5f69a9e8a5fe6a54ea26b726f590894f7
ab7ung <me@rohitjoshi.net>
2021-01-17 14:15:05 +0400

    Update kali-tools.sh

--- kali-tools.sh
+++ kali-tools.sh
@@ -71,4 +71,5 @@ sudo apt-get install -y python3-venv python3-impacket
 python3 -m pip install --user pipx
 pipx install witnessme
 pipx install git+https://github.com/Tib3rius/AutoRecon.git
+pipx install git+https://github.com/laramies/theHarvester.git
 sudo gem install evil-winrm
