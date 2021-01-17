9069be65503d565015fca52692b2df443eabf9aa
ab7ung <me@rohitjoshi.net>
2021-01-17 14:56:44 +0400

    Delete py2envtools.sh

--- py2envtools.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#enter the py2 venv before executing
-python -m pip install --upgrade pip
-python2 -m pip install --upgrade pip
-python2 -m pip install --upgrade pwntools
-
-pip2 install requests impacket progress paramiko pycrypto 
-pip2 install crackmapexec
-pip2 install aclpwn
